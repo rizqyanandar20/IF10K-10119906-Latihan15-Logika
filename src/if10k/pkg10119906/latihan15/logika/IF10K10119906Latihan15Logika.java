6384063cabe7437b50ec5f7926e6048a7f14500b
rizqyanandar20 <rizqyananda.rusmana@gmail.com>
2020-10-21 07:07:18 +0700

    first commit

---
+++ src/if10k/pkg10119906/latihan15/logika/IF10K10119906Latihan15Logika.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119906.latihan15.logika;
+
+/**
+ *
+ * @author senenngahenen
+ * Nama      : Rizqy Ananda Rusmana
+ * NIM       : 10119906
+ * Kelas     : IF-10K
+ * Deskripsi : Logika.
+ */
+public class IF10K10119906Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        boolean a = true;
+        boolean b = false;
+        
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+        
+    }
+    
+}
